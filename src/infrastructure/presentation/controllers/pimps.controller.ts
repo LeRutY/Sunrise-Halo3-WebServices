cbd04363dc4b36ea5229757aa77c74416607f705
Codie Stella <git@codie.gg>
2022-12-28 21:11:28 +0000

    Updated endpoints for pimps.

--- src/infrastructure/presentation/controllers/pimps.controller.ts
+++ src/infrastructure/presentation/controllers/pimps.controller.ts
@@ -12,15 +12,20 @@ import { createReadStream } from 'fs';
 import { Response } from 'express';
 
 @ApiTags('Alpha Upload Server')
-@Controller('/upload_server/storage')
+@Controller('/upload_server/storage/default')
 export class PimpsController {
   @Get('/alpha_motd.txt')
   async getManifest(@Res({ passthrough: true }) res: Response) {
     return await this.sendLocalFile(`alpha_motd.txt`, res);
   }
 
+  @Get('/network_configuration_062.bin')
+  async getConfig(@Res({ passthrough: true }) res: Response) {
+    return await this.sendLocalFile(`network_configuration_062.bin`, res);
+  }
+
   private async sendLocalFile(path: string, res: Response) {
-    path = join(process.cwd(), `public/storage/pimps/`, path);
+    path = join(process.cwd(), `public/storage/title/tracked/06481`, path);
 
     const stats = await stat(path);
 
