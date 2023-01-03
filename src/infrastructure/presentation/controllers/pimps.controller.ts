2367a8d284c53fdbfe3213d88cd04508ccbd29fb
Codie Stella <git@codie.gg>
2023-01-03 14:41:32 +0000

    Small update for title support.

--- src/infrastructure/presentation/controllers/pimps.controller.ts
+++ src/infrastructure/presentation/controllers/pimps.controller.ts
@@ -10,6 +10,7 @@ import { join } from 'path';
 import { stat } from 'fs/promises';
 import { createReadStream } from 'fs';
 import { Response } from 'express';
+import { Param } from '@nestjs/common/decorators';
 
 @ApiTags('Alpha Upload Server')
 @Controller('/upload_server/storage/default')
@@ -22,6 +23,32 @@ export class PimpsController {
   @Get('/network_configuration_062.bin')
   async getConfig(@Res({ passthrough: true }) res: Response) {
     return await this.sendLocalFile(`network_configuration_062.bin`, res);
+  }
+
+  @Get('/matchmaking_hopper_descriptions_001.bin')
+  async getHopperDescriptions(@Res({ passthrough: true }) res: Response) {
+    return await this.sendLocalFile(
+      `matchmaking_hopper_descriptions_001.bin`, 
+      res,
+    );
+  }
+
+  @Get('/matchmaking_hopper_002.bin')
+  async getHoppers(@Res({ passthrough: true }) res: Response) {
+    return await this.sendLocalFile(`matchmaking_hopper_002.bin`, res);
+  }
+
+  @Get('/:hopperId/game_set_001.bin')
+  async getGameset(
+    @Param('hopperId') hopperId: string,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    return await this.sendLocalFile(`${hopperId}/game_set_001.bin`, res);
+  }
+
+  @Get('/dynamic_hopper_statistics.bin')
+  async getHopperStats(@Res({ passthrough: true }) res: Response) {
+    return await this.sendLocalFile(`dynamic_hopper_statistics.bin`, res);
   }
 
   private async sendLocalFile(path: string, res: Response) {
